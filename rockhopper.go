5dc2193c99933a5d43ac3d7f28f89c1a46416630
Reid <reid.rise@gmail.com>
2025-07-09 20:56:14 -0600

    Init Commit
    Golang naive implementation of wget

---
+++ rockhopper.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: wget-go <url> <output_file>")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+	outputFile := os.Args[2]
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching URL: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error: HTTP status code %d for URL %s\n", resp.StatusCode, url)
+		os.Exit(1)
+	}
+
+	// Redirect standard output to the output file
+	file, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Creating File: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Printf("Error saving to file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Downloaded %s to %s\n", url, outputFile)
+}
